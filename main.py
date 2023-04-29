b67b4a27402101cd7091d007fc8b39c4696542c5
Ilona Shevchenko <ilona.shevchenko@gmai.com>
2023-04-29 17:09:57 +0300

    read commit

--- main.py
+++ main.py
@@ -4,13 +4,50 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from pyspark import SparkConf
+from pyspark.sql import SparkSession, Window
+import pyspark.sql.types as t
+import pyspark.sql.functions as f
+import read as r
+
+
+def main():
+    spark_session = (SparkSession.builder
+                     .master("local")
+                     .appName("task app")
+                     .config(conf=SparkConf())
+                     .getOrCreate())
+
+    akas_df = r.read_akas(spark_session)
+    akas_df.show(truncate=False)
+
+    title_basic_df = r.read_title_basic(spark_session)
+    title_basic_df.show(truncate=False)
+
+    title_crew_df = r.read_title_crew(spark_session)
+    title_crew_df.show(truncate=False)
+
+    title_episode_df = r.read_title_episode(spark_session)
+    title_episode_df.show(truncate=False)
+
+    title_principals_df = r.read_title_principals(spark_session)
+    title_principals_df.show(truncate=False)
+
+    title_ratings_df = r.read_title_ratings(spark_session)
+    title_ratings_df.show(truncate=False)
+
+    name_basics_df = r.read_name_basics(spark_session)
+    name_basics_df.show(truncate=False)
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+
+
+
---
+++ read.py
@@ -0,0 +1,114 @@
+import pyspark.sql.types as t
+import pyspark.sql.functions as f
+
+
+def read_akas(spark_session):
+    akas_schema = t.StructType([t.StructField('titleId', t.StringType(), False),
+                                t.StructField('ordering', t.IntegerType(), False),
+                                t.StructField('title', t.StringType(), True),
+                                t.StructField('region', t.StringType(), True),
+                                t.StructField('language', t.StringType(), True),
+                                t.StructField('types', t.StringType(), True),
+                                t.StructField('attributes', t.StringType(), True),
+                                t.StructField('isOriginalTitle', t.IntegerType(), True)
+                                ])
+
+    path = 'data/title.akas.tsv.gz'
+    akas_df = spark_session.read.csv(path, sep=r'\t', header=True, nullValue='null', schema=akas_schema)
+    akas_df = akas_df.withColumn('region', f.when(f.col('region') == '\\N', None).otherwise(f.col('region')))
+    akas_df = akas_df.withColumn('language', f.when(f.col('language') == '\\N', None).otherwise(f.col('language')))
+    akas_df = akas_df.withColumn('types', f.when(f.col('types') == '\\N', None).otherwise(f.col('types')))
+    akas_df = akas_df.withColumn('attributes',
+                                 f.when(f.col('attributes') == '\\N', None).otherwise(f.col('attributes')))
+    return akas_df
+
+
+def read_title_basic(spark_session):
+    title_basic_schema = t.StructType([t.StructField('tconst', t.StringType(), False),
+                                       t.StructField('titleType', t.IntegerType(), True),
+                                       t.StructField('primaryTitle', t.StringType(), True),
+                                       t.StructField('originalTitle', t.StringType(), True),
+                                       t.StructField('isAdult', t.IntegerType(), True),
+                                       t.StructField('startYear', t.IntegerType(), True),
+                                       t.StructField('endYear', t.DateType(), True),
+                                       t.StructField('runtimeMinutes', t.IntegerType(), True),
+                                       t.StructField('genres', t.StringType(), True)
+                                       ])
+
+    path = 'data/title.basics.tsv.gz'
+    title_basic_df = spark_session.read.csv(path, sep=r'\t', header=True, nullValue='null',
+                                            schema=title_basic_schema)
+    return title_basic_df
+
+
+def read_title_crew(spark_session):
+    title_crew_schema = t.StructType([t.StructField('tconst', t.StringType(), False),
+                                      t.StructField('directors', t.StringType(), True),
+                                      t.StructField('writers', t.StringType(), True)
+                                      ])
+
+    path = 'data/title.crew.tsv.gz'
+    title_crew_df = spark_session.read.csv(path, sep=r'\t', header=True, nullValue='null',
+                                           schema=title_crew_schema)
+    title_crew_df = title_crew_df.withColumn('writers', f.when(f.col('writers') == '\\N', None).
+                                             otherwise(f.col('writers')))
+    return title_crew_df
+
+
+def read_title_episode(spark_session):
+    title_episode_schema = t.StructType([t.StructField('tconst', t.StringType(), False),
+                                         t.StructField('parentTconst', t.StringType(), True),
+                                         t.StructField('seasonNumber', t.IntegerType(), True),
+                                         t.StructField('episodeNumber', t.IntegerType(), True)
+                                         ])
+
+    path = 'data/title.episode.tsv.gz'
+    title_episode_df = spark_session.read.csv(path, sep=r'\t', header=True, nullValue='null',
+                                              schema=title_episode_schema)
+    return title_episode_df
+
+
+def read_title_principals(spark_session):
+    title_principals_schema = t.StructType([t.StructField('tconst', t.StringType(), False),
+                                            t.StructField('ordering', t.IntegerType(), True),
+                                            t.StructField('nconst', t.StringType(), True),
+                                            t.StructField('category', t.StringType(), True),
+                                            t.StructField('job', t.StringType(), True),
+                                            t.StructField('characters', t.StringType(), True)
+                                            ])
+
+    path = 'data/title.principals.tsv.gz'
+    title_principals_df = spark_session.read.csv(path, sep=r'\t', header=True, nullValue='null',
+                                                 schema=title_principals_schema)
+    title_principals_df = title_principals_df.withColumn('job', f.when(f.col('job') == '\\N', None)
+                                                         .otherwise(f.col('job')))
+    title_principals_df = title_principals_df.withColumn('characters', f.when(f.col('characters') == '\\N', None)
+                                                         .otherwise(f.col('characters')))
+    return title_principals_df
+
+
+def read_title_ratings(spark_session):
+    title_ratings_schema = t.StructType([t.StructField('tconst', t.StringType(), False),
+                                         t.StructField('averageRating', t.DoubleType(), True),
+                                         t.StructField('numVotes', t.IntegerType(), True)
+                                         ])
+
+    path = 'data/title.ratings.tsv.gz'
+    title_ratings_df = spark_session.read.csv(path, sep=r'\t', header=True, nullValue='null',
+                                              schema=title_ratings_schema)
+    return title_ratings_df
+
+
+def read_name_basics(spark_session):
+    name_basics_schema = t.StructType([t.StructField('nconst', t.StringType(), False),
+                                       t.StructField('primaryName', t.StringType(), True),
+                                       t.StructField('birthYear', t.IntegerType(), True),
+                                       t.StructField('deathYear', t.IntegerType(), True),
+                                       t.StructField('primaryProfession', t.StringType(), True),
+                                       t.StructField('knownForTitles', t.StringType(), True)
+                                       ])
+
+    path = 'data/name.basics.tsv.gz'
+    name_basics_df = spark_session.read.csv(path, sep=r'\t', header=True, nullValue='null',
+                                            schema=name_basics_schema)
+    return name_basics_df
