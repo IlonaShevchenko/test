5aa530cafade96d454543758c74212ca03176f1c
Ilona Shevchenko <ilona.shevchenko@gmai.com>
2023-04-30 20:51:35 +0300

    task4_show

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import read as r
 import task1 as t1
 import task2 as t2
 import task3 as t3
+import task4 as t4
 
 def main():
     spark_session = (SparkSession.builder
@@ -21,31 +22,33 @@ def main():
                      .getOrCreate())
 
     title_akas_df = r.read_akas(spark_session)
-    title_akas_df.show(truncate=False)
+    #title_akas_df.show(truncate=False)
 
     title_basics_df = r.read_title_basic(spark_session)
-    title_basics_df.show(truncate=False)
+    #title_basics_df.show(truncate=False)
 
     title_crew_df = r.read_title_crew(spark_session)
-    title_crew_df.show(truncate=False)
+    #title_crew_df.show(truncate=False)
 
     title_episode_df = r.read_title_episode(spark_session)
-    title_episode_df.show(truncate=False)
+    #title_episode_df.show(truncate=False)
 
     title_principals_df = r.read_title_principals(spark_session)
-    title_principals_df.show(truncate=False)
+    #title_principals_df.show(truncate=False)
 
     title_ratings_df = r.read_title_ratings(spark_session)
-    title_ratings_df.show(truncate=False)
+    #title_ratings_df.show(truncate=False)
 
     name_basics_df = r.read_name_basics(spark_session)
-    name_basics_df.show(truncate=False)
+    #name_basics_df.show(truncate=False)
 
     t1.task1(title_akas_df)
 
     t2.task2(name_basics_df, 19)
 
     t3.task3(title_basics_df, 'movie', 120)
+
+    t4.task4(title_principals_df, name_basics_df, title_basics_df)
 
 
 
--- task1.py
+++ task1.py
@@ -4,5 +4,5 @@ import pyspark.sql.functions as f
 def task1(title_akas_df):
     path = '/result/result1'
     result_df = title_akas_df.filter(f.col('region') == 'UA').select('title', 'region')
-    # result_df.write.csv(path, header=True, mode='overwrite')
+    #result_df.write.csv(path, header=True, mode='overwrite')
     result_df.show()
